613a2924da8b6ec3960491134e9a94eca3d58c26
Ibrocalculus <ibsuleiman9@gmail.com>
2026-03-31 15:17:22 +0100

    Commit No.10

--- express_js/_basics.js
+++ express_js/_basics.js
@@ -1,17 +1,50 @@
 // npm i express
 
 const express = require('express');
-
 const app = express();
+const IP = "127.0.0.1";
+const PORT = 3001;
 
+
+// Middleware to parse JSON bodies (For post requests)
+app.use(express.json());
+
+
+// ====================== RETURNING STRING RESPONSE =============
 app.get('/', (req, res) => {
     res.send('Hello Ibrahim! You are running');
     }
 );
 
-app.listen(3002, '127.0.0.1', () => {
-    console.log('Listening on  127.0.0.1:3002');
+// ====================== RETURNING JSON RESPONSE ===========
+const jsonData = {
+  name: "Ibrahim",
+  age: 25,
+  city: "Cairo",
+  occupation: "Software Engineer",
+};
+
+app.get("/get_json", (req, res) => {
+  res.json(jsonData);
+});
+
+app.listen(PORT, IP, () => {
+    console.log(`Listening on  ${IP}:${PORT}`);
     }
 );
+
+// ====================== JSON POST REQUEST ===========
+app.post('/api/user', (req, res) => {
+  // Access the data sent by the user via req.body
+  const receivedData = req.body;
+  
+  console.log("Data received:", receivedData);
+
+  // Send a response back confirming receipt
+  res.json({
+    message: "User created successfully",
+    user: receivedData
+  });
+});
 
 // Run same way. ie node _basics or nodemon _basics
